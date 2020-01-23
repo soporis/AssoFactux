8934fa089c632fc491cb4802a4c6bde469ab5881
thomas iaconelli <thomas.iaconelli@gmail.com>
2020-01-23 21:12:02 +0100

    premier commit

--- activer_lot.php
+++ activer_lot.php
@@ -1,27 +1,28 @@
-<?php 
+<?php
 /*
- * Factux le facturier libre
- * Copyright (C) 2003-2005 Guy Hendrickx, 2017 Thomas Ingles
+ *  Logiciel AssoFactux 
+ *  Fork du code php Factux le facturier libre
+ *  Crée par 2003-2005 Guy Hendrickx, 2017 Thomas Ingles
  * 
  * Licensed under the terms of the GNU  General Public License:
  * 		http://opensource.org/licenses/GPL-3.0
  * 
  * For further information visit:
- * 		http://factux.free.fr
+ * 		http://
  * 
  * File Name: activer_lot.php
  * 	desactive les lot a la demande.
  * 
- * * * Version:  5.0.0
+ * * * Version:  5.0.1
  * * * * Modified: 07/10/2016
  * 
  * File Authors:
- * 		Guy Hendrickx
+ * 		thomas iaconelli
  *
  */
 include_once("include/config/common.php");
-$num=isset($_GET['num'])?$_GET['num']:"";
-$acte=isset($_GET['acte'])?$_GET['acte']:"non";
-$sql2 = "UPDATE " . $tblpref ."lot SET actif='".$acte."' WHERE num = '".$num."'";
-mysql_query($sql2) OR die("<p>Erreur Mysql<br/>$sql2<br/>".mysql_error()."</p>");
+$num = isset($_GET['num']) ? $_GET['num'] : "";
+$acte = isset($_GET['acte']) ? $_GET['acte'] : "non";
+$sql2 = "UPDATE " . $tblpref . "lot SET actif='" . $acte . "' WHERE num = '" . $num . "'";
+mysql_query($sql2) or die("<p>Erreur Mysql<br/>$sql2<br/>" . mysql_error() . "</p>");
 include_once("lister_lot.php");
